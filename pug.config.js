f80e1ccc6f9ce8fcdd9037bb65177555fe634a55
Laura Brown <laurabrown1113@gmail.com>
2019-08-09 15:03:48 +0200

    update images, move config to yml

--- pug.config.js
+++ pug.config.js
@@ -1,45 +1,7 @@
+yaml = require('js-yaml');
+fs   = require('fs');
+
+var settings = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf8'));
 module.exports = {
-  locals: {
-    general: {
-      title: "Patrick Laing",
-      metaDescription: "The website of Patrick Laing",
-      metaImage: ""
-    },
-    left: {
-      title: "Products",
-      imagePath: 'assets/images/left/',
-      videos: [
-        'https://player.vimeo.com/external/350055522.hd.mp4?s=bf43452ccb38efd5ccae982f9a726748b97c9ff0&profile_id=174',
-        'https://player.vimeo.com/external/350055495.hd.mp4?s=77485a32b2cdfeaa26fa5869cdf2032751ad1c10&profile_id=175'
-      ],
-      images: [{
-        fileName:'pl_website_statue_j.jpg'
-      }]
-    },
-    right: {
-      title: "Rings",
-      imagePath: 'assets/images/right/',
-      videos: [
-        'https://player.vimeo.com/external/350055538.hd.mp4?s=ca47fde229ed29e738ea5855338805bb177087ba&profile_id=174'
-      ],
-      images: [{
-        fileName: 'pl_website_statue_i.jpg',
-        cover: true
-      }]
-    },
-    contact: {
-      linkText: "Contact",
-      email: "patrick@patricklaing.com"
-    },
-    credits: [
-      {
-        linkText: "Design by Ongoing Projects",
-        linkDestination: "https://ongoingprojects.co"
-      },
-      {
-        linkText: "Built by Laura Brown",
-        linkDestination: "https://laurabrown.xyz"
-      }
-    ]
-  }
+  locals: settings
 };
