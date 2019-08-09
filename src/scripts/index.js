41b73964cb99b84f7e6edd92f641aad3c29802be
Laura Brown <laurabrown1113@gmail.com>
2019-08-09 18:14:36 +0200

    preload images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -24,10 +24,12 @@ const selectors = {
 }
 var $container;
 
-function imageDuration() {
-  return Math.floor((3 * Math.random() + 2) * 1000)
-}
+var nextMedia;
 
+
+function isDesktop() {
+  return window.outerWidth >= 768;
+}
 function updateSubtitle (e) {
   const newTitle = e.currentTarget.getAttribute('data-title')
   fadeSubtitle(newTitle)
@@ -100,38 +102,69 @@ function didClick(e) {
   }
 }
 
-function displayImage(el, context) {
-  $(el).addClass(classes.activeMediaItem);
-  setTimeout(cycleMedia.bind(context), imageDuration())
-}
+class CycleMedia {
 
-function displayVideo(el, context) {
-  $(el).addClass(classes.activeMediaItem);
-  $(el).one('ended', cycleMedia.bind(context))
-  el.play()
-}
+  constructor(el) {
+    this.$el = $(el)
+    this.nextMedia = this.getNextMedia()
+  }
+  imageDuration() {
+    return Math.floor((3 * Math.random() + 4) * 1000)
+  }
+  preloadImage(url) {
+    var img=new Image();
+    img.src=url;
+  }
+  cycle() {
+    this.displayNextMedia()
+    this.nextMedia = this.getNextMedia()
+  }
 
-function cycleMedia() {
-  var $mediaItems = $(this).children().filter((index, el) => {
-    if (el.matches(selectors.video)  ) { return false; }
-    return true;
-  })
+  displayImage(el) {
+    $(el).addClass(classes.activeMediaItem);
+    setTimeout(this.cycle.bind(this), this.imageDuration())
+  }
 
-  if ($mediaItems.length > 1) {
-    $mediaItems = $mediaItems.filter((index, el) => {
-      if (el.matches(selectors.activeMediaItem)) { return false; }
+  displayVideo(el) {
+    $(el).addClass(classes.activeMediaItem);
+    $(el).one('ended', this.cycle.bind(this))
+    el.play()
+  }
+  displayNextMedia() {
+    this.$el.children().removeClass(classes.activeMediaItem)
+    if (this.nextMedia.matches(selectors.image)) {
+      this.displayImage(this.nextMedia)
+    } else if (this.nextMedia.matches(selectors.video)) {
+      this.displayVideo(this.nextMedia)
+    }
+  }
+  getNextMedia() {
+    var $mediaItems = this.$el.children().filter((index, el) => {
+      if (el.matches(selectors.video) && el.readyState < 3) { return false; }
       return true;
     })
-  }
-  const randomIndex = Math.floor(Math.random() * $mediaItems.length)
-  const random = $mediaItems[randomIndex];
-  $(this.children).removeClass(classes.activeMediaItem)
-  if (random.matches(selectors.image)) {
-    displayImage(random, this)
-  } else if (random.matches(selectors.video)) {
-    displayVideo(random, this)
+
+    if ($mediaItems.length > 1) {
+      $mediaItems = $mediaItems.filter((index, el) => {
+        if (el.matches(selectors.activeMediaItem)) { return false; }
+        return true;
+      })
+    }
+    const randomIndex = Math.floor(Math.random() * $mediaItems.length)
+    const media = $mediaItems[randomIndex]
+    if (media.matches(selectors.image)) {
+      const imageSelector = `${(isDesktop() ? '.desktop' : '.mobile')} .image`
+      var url = $(media).find(imageSelector).css('background-image');
+      // ^ Either "none" or url("...urlhere..")
+      url = /^url\((['"]?)(.*)\1\)$/.exec(url);
+      url = url ? url[2] : ""; // If matched, retrieve url, otherwise ""
+      this.preloadImage(url)
+    }
+    return media;
   }
 }
+
+
 
 $(document).ready(() => {
   // codedBy();
@@ -145,5 +178,7 @@ $(document).ready(() => {
     el.load();
   })
   $container.on('click', selectors.half, didClick)
-  $(selectors.media, $container).each(cycleMedia)
+  $(selectors.media, $container).each(function() {
+    new CycleMedia(this).cycle()
+  })
 })
